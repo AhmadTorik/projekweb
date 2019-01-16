d2fcd75546472c9346c683e7c7bec65fe72591da
AhmadTorik <TorikTampan@gmail.com>
2019-01-16 13:38:50 +0700

    projek

---
+++ application/views/beranda.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Beranda</title>
+    <link rel="stylesheet" href="<?=base_url() ?>assets/style.css">
+</head>
+<body>
+    <header class="ob">
+    <div class="atas">
+    <div id="logo"><img style="width: 125px;margin-left: 20px;float: left;"
+    src="<?=base_url()?>assets/gambar.png" alt=""></div>
+    <nav>
+    <ul>
+    <li> <a href="<?=base_url()?>index.php/welcome/utama"> Home </a></li>
+    <li> <a href="<?=base_url()?>index.php/welcome/event"> Event </a></li>
+    <li> <a href="<?=base_url()?>index.php/welcome/galery"> Gallery </a></li>
+    <li> <a href="<?=base_url()?>index.php/welcome/contact"> Contact </a></li>
+    <li> <a href="<?=base_url()?>index.php/welcome/profil"> MyProfil </a></li>
+    </ul>
+</nav>
+</div>
+    </header>
+    <h1 align="center" style="color:skyblue;text-align: left;font-size: 10.3mm;margin-top: 97px; margin-left: 12px; font-family: sans-serif;">
+      Selamat Datang Torik
+    <br> Berperilakulah Seperti Cowok</h1>
+
+    <p style="font-size: 40px;margin-left: 15px;margin-top: 39px;">Keep Spirit</p>
+    <img style="width: 823px;margin-left: 518px;margin-top: -312px;}" src="assets/back.png" alt="">
+</body>
+</html>
---
+++ application/views/profil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Profil ADMIN</title>
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/profil.css">
+    </head>
+    <body style=" text-align: center;">
+
+
+            <h1>Torik</h1>
+            <h5>
+                Web Developer
+            </h5>
+            <p>Salam kenal</p>
+            <p>Semoga Bahagia</p>
+            <p>Hubungi kontak saya : 083847006777</p>
+        
+
+    </body>
+</html>
